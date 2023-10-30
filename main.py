a1e7a91de24f0517b6266e6af8a74f86280fcc5a
alainhortiz <alainhortiz@gmail.com>
2023-10-30 09:40:51 -0400

    Condicionales

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-nombre = 'Alain'
+nombre = input('¿Como te llamas?')
 print('Hola Mundo, soy ' + nombre)
+
+edad = int(input('¿Que edad tienes?'))
+if edad > 20:
+    print('Eres mayor de edad')
+else:
+    print('Eres menor de edad')
