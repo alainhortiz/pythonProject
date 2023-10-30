0da810e498c7733c8b6828d54bd1e12db2e395a9
alainhortiz <alainhortiz@gmail.com>
2023-10-30 09:56:35 -0400

    Operates arithmetic

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ if edad > 20:
     print('Eres mayor de edad')
 else:
     print('Eres menor de edad')
+
+numero = int(input('Ingrese un numero'))
+if numero % 2 == 0:
+    print('El numero es par')
+else:
+    print('El numero es impar')
